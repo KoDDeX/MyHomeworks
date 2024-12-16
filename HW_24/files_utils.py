85900702599c9f06f1a74e311d603ebed2653600
Ivan Bunko <kodexx@bk.ru>
2024-12-16 23:03:53 +0300

    HW_24. Добавил файл проверки. Проверка JSON и CSV. Наладил работу функций JSON и CSV.

--- HW_24/files_utils.py
+++ HW_24/files_utils.py
@@ -26,21 +26,25 @@ def read_json(file_path: str, encoding: str = 'utf-8-sig') -> dict:
         with open(file_path, 'r', encoding=encoding) as file:
             return json.load(file)
 
-def write_json(data, file_path: str, encoding: str = "utf-8-sig") -> None:
+def write_json(data: list[dict], file_path: str, encoding: str = "utf-8-sig") -> None:
     """"
     Функция для записи данных в JSON-файл.
     """
-    if file_exist(file_path):
-        with open(file_path, 'w', encoding=encoding) as file:
-            json.dump(data, file, indent=4, ensure_ascii=False)
+    # if file_exist(file_path):
+    with open(file_path, 'w', encoding=encoding) as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
 
 def append_json(data: list[dict], file_path: str, encoding: str = "utf-8-sig") -> None:
     """
     Функция для добавления данных в существующий JSON-файл
     """
     if file_exist(file_path):
-        with open(file_path, 'a', encoding=encoding) as file:
-            json.dump(data, file, indent=4, ensure_ascii=False)
+        with open(file_path, 'r', encoding=encoding) as file:
+            json_data = json.load(file)
+        json_data.extend(data)
+        with open(file_path, 'w', encoding=encoding) as file:
+            json.dump(json_data, file, indent=4, ensure_ascii=False)
+        
 
 def read_csv(file_path: str, delimiter: str = ';', encoding: str = 'utf-8-sig') -> list:
     """
@@ -51,22 +55,26 @@ def read_csv(file_path: str, delimiter: str = ';', encoding: str = 'utf-8-sig') 
             table_list = list(csv.reader(file, delimiter=delimiter))
             return table_list
 
-def write_csv(data: list, file_path: str, delimiter: str = ';', encoding: str = 'utf-8-sig') -> None:
+def write_csv(data: list[dict], file_path: str, delimiter: str = ';', encoding: str = 'utf-8-sig') -> None:
     """
     Функция для записи данных в CSV-файл.
     """
-    with open(file_path, 'w', encoding=encoding) as file:
+    with open(file_path, 'w', encoding=encoding, newline = "") as file:
         writer = csv.writer(file, delimiter=delimiter)
-        writer.writerows(data)
+        writer.writerow(data[0])
+        for row in data: 
+            writer = csv.DictWriter(file, fieldnames=row.keys(), delimiter=delimiter)
+            writer.writerow(row)
 
-def append_csv(data: list, file_path: str, delimiter: str = ';', encoding: str = 'utf-8-sig') -> None:
+def append_csv(data: list[dict], file_path: str, delimiter: str = ';', encoding: str = 'utf-8-sig') -> None:
     """
     Функция для добавления данных в существующий CSV-файл
     """
     if file_exist(file_path):
-        with open(file_path, 'a', encoding=encoding) as file:
-            writer = csv.writer(file, delimiter=delimiter)
-            writer.writerows(data)
+        with open(file_path, 'a', encoding=encoding, newline= "") as file:
+            for row in data:
+                writer = csv.DictWriter(file, fieldnames=row.keys(), delimiter=delimiter)
+                writer.writerow(row)
 
 def read_txt(file_path: str, encoding: str = 'utf-8-sig') -> str:
     """
@@ -91,7 +99,7 @@ def append_txt(data: str, file_path: str, encoding: str = 'utf-8-sig') -> None:
         with open(file_path, 'a', encoding=encoding) as file:
             file.write(data)
 
-def read_yaml(file_path: str, encoding: str = 'utf-8-sig') -> dict:
+def read_yaml(file_path: str, encoding: str = 'utf-8-sig') -> list:
     """
     Функция для чтения данных из файла YAML.
     """
---
+++ HW_24/files_utils_tests.py
@@ -0,0 +1,43 @@
+import files_utils
+
+table_list = [
+    {"first_name": "Владимир", "middle_name": "Александрович", "last_name": "Монин"},
+    {"first_name": "Семен", "middle_name": "Константинович", "last_name": "Беляев"},
+    {"first_name": "Дмитрий", "middle_name": "Владимирович", "last_name": "Бобов"},
+    {"first_name": "Иван", "middle_name": "Петрович", "last_name": "Бунько"},
+    {"first_name": "Никита", "middle_name": "Федорович", "last_name": "Вахрамеев"},
+    {"first_name": "Екатерина", "middle_name": "Александровна", "last_name": "Голосняк"},
+    {"first_name": "Спартак", "middle_name": "Витальевич", "last_name": "Добровольский"},
+    {"first_name": "Григорий", "middle_name": "Сергеевич", "last_name": "Калинин"},
+    {"first_name": "Вадим", "middle_name": "Валерьевич", "last_name": "Козлов"},
+    {"first_name": "Андрей", "middle_name": "Викторович", "last_name": "Курочкин"},
+    {"first_name": "Размик", "middle_name": "Априкович", "last_name": "Мнацаканян"},
+    {"first_name": "Алексей", "middle_name": "Николаевич", "last_name": "Охонько"},
+    {"first_name": "Даниил", "middle_name": "Дмитриевич", "last_name": "Рукавишников"},
+    {"first_name": "Алексей", "middle_name": "Владимирович", "last_name": "Черноусов"},
+    {"first_name": "Павел", "middle_name": "Алексеевич", "last_name": "Шарапов"},
+    {"first_name": "Кирилл", "middle_name": "Русланович", "last_name": "Шарахудинов"},
+    {"first_name": "Дмитрий", "middle_name": "Вячеславович", "last_name": "Юдин"}
+]
+
+append_list = [
+    {"first_name": "Степан", "middle_name": "Степанович", "last_name": "Степанов"},
+    {"first_name": "Иван", "middle_name": "Иванович", "last_name": "Иванов"},
+    {"first_name": "Петр", "middle_name": "Петрович", "last_name": "Петров"},
+]
+
+# Тестируем JSON
+# files_utils.write_json(table_list, 'test.json')
+# test_json = files_utils.read_json('test.json')
+# print(test_json)
+# files_utils.append_json(append_list, 'test.json')
+# test_json = files_utils.read_json('test.json')
+# print(test_json)
+
+# Тестируем CSV
+files_utils.write_csv(table_list, 'test.csv')
+test_csv = files_utils.read_csv('test.csv')
+print(test_csv)
+files_utils.append_csv(append_list, 'test.csv')
+test_csv = files_utils.read_csv('test.csv')
+print(test_csv)
