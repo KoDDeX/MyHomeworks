0f2593f7fe4b83ff7da2ca1b0f0fffc685117886
Ivan Bunko <kodexx@bk.ru>
2024-12-21 23:41:38 +0300

    HW_25. Создал словарь с помощью filter

--- HW_25/HW_25.py
+++ HW_25/HW_25.py
@@ -1,4 +1,7 @@
 from marvel import full_dict
 
-lst = list(map(lambda x: int(x) if "0" <= x <= "9" else None, input("Введите цифры через пробел: ").split()))
+user_input = list(map(lambda x: int(x) if "0" <= x <= "9" else None, input("Введите цифры через пробел: ").split()))
+
+# lst = list(filter(lambda x, y: y['title'] if x in user_input else None, full_dict.keys()))
+lst = dict(filter(lambda x: x[0] in user_input, full_dict.items()))
 
