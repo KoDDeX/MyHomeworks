180d8f20fda5a4bcec08ea6a0042d7272c3eeb4e
Ivan Bunko <kodexx@bk.ru>
2024-12-23 22:15:06 +0300

    HW_25. Создал множество по Режисеру.  Создал копию словаря с текстовым полем year. Получил словарь с фильмами на Ч. Отсортировал словарь по названиям фильмов.

--- HW_25/HW_25.py
+++ HW_25/HW_25.py
@@ -1,7 +1,21 @@
 from marvel import full_dict
+from pprint import pprint
 
 user_input = list(map(lambda x: int(x) if "0" <= x <= "9" else None, input("Введите цифры через пробел: ").split()))
+# pprint(user_input)
 
-# lst = list(filter(lambda x, y: y['title'] if x in user_input else None, full_dict.keys()))
-lst = dict(filter(lambda x: x[0] in user_input, full_dict.items()))
+films_dict = dict(filter(lambda x: x[0] in user_input, full_dict.items()))
+# pprint(films_dict)
 
+director_set = {val['director'] for val in films_dict.values()}
+# pprint(f'Режиссёры: {", ".join(director_set)}')
+
+full_dict_years = {k:{k1:str(v1) for (k1, v1) in v.items()} for (k, v) in full_dict.items()}
+# pprint(full_dict_years)
+
+full_dict_che = dict(filter(lambda x: x[1]['title'][0].lower() == 'ч', full_dict_years.items()))
+# pprint(full_dict_che)
+
+# Отсортировал словарь по названию фильма ['title']
+full_dict_sorted = dict(sorted(full_dict.items(), key=lambda x: x[1]['title'] if x[1]['title'] != None else ''))
+# pprint(full_dict_sorted, sort_dicts=False)
