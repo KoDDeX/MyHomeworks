c9bf432b65e74717a3b984d100b455afd0e60ef0
Ivan Bunko <kodexx@bk.ru>
2025-01-23 20:56:48 +0300

    HW_28. Добавил метод записи файла

--- HW_28/HW_28.py
+++ HW_28/HW_28.py
@@ -23,3 +23,18 @@ class TxtFileHandler:
         with open(file_path, "r", encoding=encoding) as file:
             return file.read()
 
+    def write_file(
+        self, file_path: str, data: str, encoding: str = "utf-8-sig"
+    ) -> None:
+        """
+        Метод для записи данных в TXT файл.
+        :param file_path: str - Путь к файлу.
+        :param data: str - Данные для записи.
+        :param encoding: str - Кодировка файла.
+        """
+        try:
+            with open(file_path, "w", encoding=encoding) as file:
+                file.write(data)
+        except PermissionError as e:
+            raise PermissionError(f"Ошибка доступа к файлу файл: {e}")
+
