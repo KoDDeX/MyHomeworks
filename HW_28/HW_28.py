2ca7f7f830a1a9332eb6a64ee8e642522ee51c3f
Ivan Bunko <kodexx@bk.ru>
2025-01-23 20:31:19 +0300

    HW_28. Написал функцию чтения файла с проверкой

---
+++ HW_28/HW_28.py
@@ -0,0 +1,25 @@
+"""
+Домашнее задание № 28
+
+Реализация класса для работы с TXT файлами в стиле ООП
+"""
+import os
+
+
+class TxtFileHandler:
+    """
+    Класс для работы с текстовыми файлами. Этот класс предоставляет методы для чтения, записи и добавления данных в TXT файлы
+    """
+
+    def read_file(self, file_path: str, encoding: str = "utf-8-sig") -> str:
+        """
+        Метод для чтения данных из TXT файла.
+        :param file_path: str - Путь к файлу.
+        :param encoding: str - Кодировка файла.
+        :return: str - Содержимое файла.
+        """
+        if not os.path.exists(file_path):
+            return ""
+        with open(file_path, "r", encoding=encoding) as file:
+            return file.read()
+
