e6fdda0b7afa7e79265a2bfccc0b5dd415a287ff
Ivan Bunko <kodexx@bk.ru>
2025-01-26 21:16:18 +0300

    HW_29. Написал compress_image и process_directory

--- HW_29/HW_29.py
+++ HW_29/HW_29.py
@@ -1,3 +1,14 @@
+"""
+Домашнее задание №29
+Рефакторинг кода для сжатия изображений с использованием принципов ООП.
+"""
+import os
+from typing import Union
+from PIL import Image
+from pillow_heif import register_heif_opener
+
+QUALITY = 50
+
 class ImageCompressor:
     def __init__(self, quality: int):
         """
@@ -5,6 +16,7 @@ class ImageCompressor:
         :param quality: int - значение качества сжатия
         """
         self.__quality = quality
+
     def compress_image(self, input_path: str, output_path: str) -> None:
         """
         Метод для сжатия изображения и сохранения его в формате HEIF
@@ -12,14 +24,23 @@ class ImageCompressor:
         :param output_path: str - путь для сохранения сжатого изображения в формате HEIF
         :return: None
         """
-        pass
+        with Image.open(input_path) as img:
+            img.save(output_path, format="HEIF", quality=self.__quality)
+        print(f'Сжато: {input_path} -> {output_path}')
+
     def process_directory(self, directory: str) -> None:
         """
         Метод для обработки всех изображений в указанной директории и её поддиректориях
-        :param directory: str - путь к директории
+        :param directory: str - путь к директории для обработки
         :return: None
         """
-        pass
+        for root, _, files in os.walk(directory):
+            for file in files:
+                if file.lower().endswith(('.jpg', '.jpeg', '.png')):
+                    input_path = os.path.join(root, file)
+                    output_path = os.path.join(root, file)
+                    self.compress_image(input_path, output_path)
+
     @property
     def quality(self) -> int:
         """
