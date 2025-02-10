9b8d2d852d3a5e64854defb4d96b4f12983ce2c0
Ivan Bunko <kodexx@bk.ru>
2025-02-11 00:48:50 +0300

    HW_31. Написал метод хода игрока human_turn

--- HW_31/HW_31.py
+++ HW_31/HW_31.py
@@ -6,6 +6,7 @@ Homework 31
 import os
 import json
 from dataclasses import dataclass, field
+import random
 
 DATA_FILE = 'data.json'
 
@@ -67,7 +68,7 @@ class CitiesSerializer:
         cities = []
         for city_info in city_data:
             city = City(
-                name=city_info['name'],
+                name=city_info['name'].lower(),
                 population=city_info['population'],
                 subject=city_info['subject'],
                 district=city_info['district'],
@@ -78,7 +79,16 @@ class CitiesSerializer:
         return cities
     
     def get_all_cities(self) -> set:
-        return self.cities
+        return [city.name for city in self.cities]
+    
+    def get_used_cities(self) -> set:
+        return [city.name for city in self.cities if city._is_used]
+    
+    def use_city(self, city_name: str) -> None:
+        for city in self.cities:
+            if city.name == city_name:
+                city._is_used = True
+                break
 
 
 class CityGame:
@@ -87,18 +97,84 @@ class CityGame:
     """
     def __init__(self, cities: CitiesSerializer):
         self.cities = cities
+        self.current_city = None
         self.used_cities = []
+        self.winner = None
+        self.is_human_turn = None
     
+    def _dice(self) -> int:
+        return random.choice(range(1, 7))
+
+    def _first_move(self) -> bool:
+        """
+        Метод для определения первого хода игрока.
+        """
+        while True:
+            print('Определим, кто будет ходить первым. Бросаем кубик...')
+            input('Нажмите Enter, чтобы бросить кубик...')
+            human_dice = self._dice()
+            print(f'У вас выпало {human_dice}\nТеперь бросает компьютер...')
+            computer_dice = self._dice()
+            print(f'У компьютера выпало {computer_dice}')
+            if human_dice > computer_dice:
+                while not self.current_city:
+                    self.human_turn(input('Вы ходите первым. Введите название города: '))
+                return True
+            elif human_dice == computer_dice:
+                print('Ничья!')
+            else:
+                print('Компьютер ходит первым!')
+                return False
+
     def start_game(self):
-        pass
+        print('Добро пожаловать в игру "Города"!')
+        self.is_human_turn = self._first_move()
+        while not self.check_game_over():
+            if self.is_human_turn:
+                self.human_turn(input(f'Введите название города на букву "{self.current_city[-1].upper()}": '))
+            else:
+                self.computer_turn()
+                self.is_human_turn = True
+            self.check_game_over()
 
     def human_turn(self, city_input: str):
-        pass
+        """
+        Метод для хода игрока.
+        """
+        if not city_input:
+            print('Вы не ввели город!')
+            return
+        
+        if city_input == 'стоп':
+            print('Вы вышли из игры!')
+            self.winner = 'Компьютер победил!'
+            return
+        
+        if city_input.lower() not in self.cities.get_all_cities():
+                print('Такого города не существует!')
+                return
+        
+        if not self.current_city:
+            self.current_city = city_input
+            self.cities.use_city(city_input)
+            return
+
+        if city_input.lower()[0] != self.current_city[-1]:
+            print('Вы неправильно назвали город!')
+            return
+
+        if city_input in self.used_cities:
+            print('Этот город уже был назван!')
+            return
+
+        self.current_city = city_input
+        self.cities.use_city(city_input)
+        self.is_human_turn = False
 
     def computer_turn(self):
         pass
 
-    def check_game_over(self):
+    def check_game_over(self) -> bool:
         pass
 
     def save_game_state(self):
@@ -116,5 +192,6 @@ class GameManager:
 
 if __name__ == '__main__':
     game_manager = GameManager(DATA_FILE)
+    game_manager.city_game.start_game()
     # cities = game_manager.cities_serializer.get_all_cities()
     # print(cities)
