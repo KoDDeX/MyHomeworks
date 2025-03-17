118bd5ee389b7fbe39fb50236638a444e25b02d2
Ivan Bunko <kodexx@bk.ru>
2025-03-17 23:41:58 +0300

    HW_35. Выполнил все проверки

--- HW_35/HW_35.sql
+++ HW_35/HW_35.sql
@@ -109,6 +109,8 @@ CREATE TABLE IF NOT EXISTS MasterSchedule (
     status_id INTEGER NOT NULL,
     comment TEXT,
     FOREIGN KEY (master_id) REFERENCES Masters(id)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (status_id) REFERENCES StatusDictionary(status_id)
     ON DELETE CASCADE ON UPDATE CASCADE
 );
 
@@ -125,7 +127,10 @@ INSERT INTO StatusDictionary
 (name, description)
 VALUES 
     ('Подтверждена', 'Запись подтверждена'),
-    ('Отменена', 'Запись отменена');
+    ('Отменена', 'Запись отменена'),
+    ('Выполнена', 'Запись выполнена'),
+    ('Работает', 'Рабочий день'),
+    ('Выходной', 'Выходной день');
 
 INSERT INTO Appointments (name, phone, date, master_id, start_time, end_time, status_id)
 VALUES (
@@ -149,5 +154,150 @@ VALUES (
     (SELECT id FROM Appointments WHERE name = 'Вася Васечкин'),
     (SELECT id FROM Services WHERE title = 'Стрижка бороды')
 );
+
+COMMIT;
+
+-- 2. Изменение статуса записи 
+    -- Создать транзакцию, которая обновит запись в таблице Appointments:
+    
+    -- - Заменить статус старого значения на новый, изменив поле status_id.
+    -- - Если запись имеет связанные элементы в Reviews (например, автоматически установить отрицательную оценку при отмене), обновите их.
+    -- - Обеспечьте корректный откат в случае возникновения проблем.
+
+BEGIN TRANSACTION;
+
+UPDATE Appointments
+SET status_id = (SELECT status_id FROM StatusDictionary WHERE name = 'Отменена')
+WHERE id = (SELECT id FROM Appointments WHERE name = 'Васька Филипенко');
+
+INSERT INTO Reviews (appointment_id, rating, comment)
+VALUES (
+    (SELECT id FROM Appointments WHERE name = 'Васька Филипенко'),
+    1,
+    'Парикмахер скотина - опять нажрался'
+);
+
+UPDATE Appointments
+SET status_id = (SELECT status_id FROM StatusDictionary WHERE name = 'Подтверждена')
+WHERE id = (SELECT id FROM Appointments WHERE name = 'Иешуа Га-Ноцри');
+
+UPDATE Appointments
+SET status_id = (SELECT status_id FROM StatusDictionary WHERE name = 'Подтверждена')
+WHERE id = (SELECT id FROM Appointments WHERE name = 'Алиса');
+
+UPDATE Appointments
+SET status_id = (SELECT status_id FROM StatusDictionary WHERE name = 'Подтверждена')
+WHERE id = (SELECT id FROM Appointments WHERE name = 'Елизавета Васильевна');
+
+COMMIT;
+
+-- 3. Корректировка цены услуги
+    -- Сделать транзакцию по обновлению цены услуги:
+    
+    -- - Обновить базовую цену в таблице Services.
+    -- - Обновить индивидуальные цены в таблице masters_services для данной услуги (при необходимости).
+    -- - Проверить, что транзакция фиксируется только при успешном выполнении всех шагов.
+
+BEGIN TRANSACTION;
+
+UPDATE Services
+SET price = 1500
+WHERE title = 'Стрижка детская';
+
+UPDATE MastersServices
+SET price = 1750
+WHERE service_id = (SELECT id FROM Services WHERE title = 'Стрижка детская')
+AND master_id = (SELECT id FROM Masters WHERE last_name = 'Иванов' AND first_name = 'Иван');    
+
+COMMIT;
+
+-- 4. Обновление расписания мастера
+    -- Создать транзакцию для изменения расписания мастера в таблице MasterSchedule:
+    
+    -- - Обновить поля start_time и end_time для выбранного мастера.
+    -- - При этом можно добавить проверку – например, чтобы новый интервал не пересекался с уже существующими.
+    -- - В случае ошибки провести ROLLBACK.
+
+BEGIN TRANSACTION;
+
+INSERT INTO MasterSchedule (master_id, day_of_week, start_time, end_time, status_id, comment)
+VALUES (
+    1,
+    1,
+    '15:00',
+    '17:00',
+    (SELECT status_id FROM StatusDictionary WHERE name = 'Работает'),
+    'Работаем с 15 до 17'
+);
+
+UPDATE MasterSchedule
+SET start_time = '13:00', end_time = '15:00'
+WHERE master_id = 1 AND day_of_week = 1;
+
+COMMIT
+
+-- 5. Добавление нового статуса 
+    -- Реализуйте транзакцию для массовой вставки новой записи в таблицу StatusDictionary:
+    
+    -- - Выполните вставку нового статуса с уникальным именем.
+    -- - Если имя статуса уже существует, откатите транзакцию и выведите сообщение об ошибке.
+
+BEGIN TRANSACTION;
+
+INSERT INTO StatusDictionary (name, description)
+VALUES ('Уволился', 'Больше не работает');
+
+COMMIT;
+
+-- 6. Добавление отзыва клиента
+    -- В транзакции:
+    
+    -- - Добавьте новую запись в таблицу Reviews для конкретной записи из Appointments.
+    -- - При этом проверьте, что рейтинг находится в допустимом диапазоне (от 1 до 5) и все внешние ключи корректны.
+    -- - Зафиксируйте вставку только если все условия выполнены.
+
+BEGIN TRANSACTION;
+
+INSERT INTO Reviews (appointment_id, rating, comment)
+VALUES (
+    (SELECT id FROM Appointments WHERE name = 'Вася Васечкин'),
+    5,
+    'Отличная работа, рекомендую!'
+    );
+    
+COMMIT;
+
+-- 7. Массовая вставка новых услуг
+
+    -- Создайте транзакцию для пакетного добавления нескольких новых записей в таблицу Services:
+    
+    -- - Вставьте 2–3 услуги за одну транзакцию.
+    -- - Проверьте, что для каждой услуги поле `duration_minutes` заполнено, а название услуги уникально.
+    -- - В случае ошибки – отмените вставку всех услуг.
+
+BEGIN TRANSACTION;
+
+INSERT INTO Services (title, description, duration_minutes, price)
+VALUES ('Маникюр', 'Маникюр с покрытием', 45, 1000),
+       ('Педикюр', 'Педикюр с покрытием', 60, 1500);
+
+COMMIT;
+
+-- 8. Отмена записи на услугу
+
+    -- Сформируйте транзакцию для отмены уже существующей записи:
+    
+    -- - Измените status_id записи в таблице Appointments на значение, соответствующее статусу «Отменена» из таблицы StatusDictionary.
+    -- - Удалите связанные записи в таблице appointments_services, если это необходимо (например, если отмена записи подразумевает удаление услуг).
+    -- - Зафиксируйте изменения или выполните откат при ошибке.
+
+BEGIN TRANSACTION;
+
+UPDATE Appointments
+SET status_id = (SELECT status_id FROM StatusDictionary WHERE name = 'Отменена')
+WHERE id = 1;
+
+DELETE FROM AppointmentsServices
+WHERE appointment_id = 1;
 
 COMMIT;
